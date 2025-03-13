c78bc6c5df43c992ecef47fc819f06d4b9bee098
Judge <abhyudayamansingh5@gmail.com>
2025-03-13 21:26:53 +0545

    added continuous right movement + fly

--- Scripts/raven.gd
+++ Scripts/raven.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 300
+@export var speed = 0.3
 @export var jump = -300
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -10,4 +10,7 @@ func _ready() -> void:
 	
 func _physics_process(delta: float) -> void:
 	self.velocity.y += gravity * delta
+	if Input.is_action_just_pressed("Fly"):
+		self.velocity.y = jump
+	self.velocity.x += speed 
 	move_and_slide()
