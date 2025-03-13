e321050b137d834cc7beab390e72738a2f4a35bc
Judge <abhyudayamansingh5@gmail.com>
2025-03-13 21:40:04 +0545

    added more chunks, made velocity.x = constant instead of += constant

--- Scripts/raven.gd
+++ Scripts/raven.gd
@@ -1,16 +1,16 @@
 extends CharacterBody2D
 
-@export var speed = 0.3
+@export var speed = 300
 @export var jump = -300
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready() -> void:
-	pass
+	self.velocity.x = speed 
 	
 func _physics_process(delta: float) -> void:
 	self.velocity.y += gravity * delta
 	if Input.is_action_just_pressed("Fly"):
 		self.velocity.y = jump
-	self.velocity.x += speed 
+	
 	move_and_slide()
