cf012af7a33c9d3bc028ddd043b660f1334a389d
Judge <abhyudayamansingh5@gmail.com>
2025-03-13 22:04:04 +0545

    adjusted speed and acceleration of birb

--- Scripts/raven.gd
+++ Scripts/raven.gd
@@ -1,16 +1,14 @@
 extends CharacterBody2D
 
-@export var speed = 50
+@export var max_speed = 500
 @export var jump = -300
-
+@export var acceleration = 50
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _ready() -> void:
-	self.velocity.x = speed 
 	
 func _physics_process(delta: float) -> void:
 	self.velocity.y += gravity * delta
 	if Input.is_action_just_pressed("Fly"):
 		self.velocity.y = jump
-	
+	self.velocity.x = min(self.velocity.x + acceleration * delta, max_speed)
 	move_and_slide()
