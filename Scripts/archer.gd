fb6381f6d0f8c03f326585fd21105cc6e8bd568f
Judge <abhyudayamansingh5@gmail.com>
2025-03-17 22:01:03 +0545

    took out archer logic, will use FSM

--- Scripts/archer.gd
+++ Scripts/archer.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var player: CharacterBody2D = get_node("../../Raven")
+
+ 
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var random_number
 
@@ -10,21 +13,9 @@ func _ready() -> void:
 	var random_archer = "archer" + random_number + "_idle"
 	sprite.play(random_archer)
 
-
 	
 func _physics_process(delta: float) -> void:
 	self.velocity.y += gravity * delta
 	sprite.flip_h = player.global_position.x > self.global_position.x
 		
 	move_and_slide()
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	var random_archer = "archer" + random_number + "_shoot"
-	sprite.play(random_archer)
-	pass
-
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	var random_archer = "archer" + random_number + "_idle"
-	sprite.play(random_archer)
