6d354a1e85d10282115bcbbac345aecc78aa3003
Judge <abhyudayamansingh5@gmail.com>
2025-03-28 22:40:58 +0545

    removed timer, added on_frame_changed, tried to fix direction of arrows, wtf

--- Scripts/archer.gd
+++ Scripts/archer.gd
@@ -3,14 +3,15 @@ extends CharacterBody2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var player: CharacterBody2D = get_node("../../Raven")
 @onready var state_chart:StateChart = $StateChart
-@onready var attack_timer: Timer = $AttackTimer
+
  
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var random_number
-var can_attack = false
+
 var arrows = []
-var arrow_speed = 500
+var arrow_speed = 400
+
 
 
 func _ready() -> void:
@@ -23,25 +24,36 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		self.velocity.y += gravity * delta
 	sprite.flip_h = player.global_position.x > self.global_position.x
+	
 	#print(can_attack)
 	move_and_slide()
 
 func shoot_arrow():
-	if can_attack:
-		var new_arrow:PackedScene = load("res://Scenes/arrow.tscn")
-		var arrow:RigidBody2D = new_arrow.instantiate()
+	var new_arrow:PackedScene = load("res://Scenes/arrow.tscn")
+	var arrow:RigidBody2D = new_arrow.instantiate()
+	var distance:Vector2 = player.global_position - $Marker2D.global_position
+	
+	if self.sprite.flip_h:
+		$Marker2D.position.x = -25.333
+		arrow.rotation = PI / 2 - atan(distance.y / distance.x)
+	else:
+		arrow.rotation = PI / 2 - atan(distance.y / distance.x)
+
 		
-		arrows.append(arrow)
-		call_deferred("add_child", arrow)
-		var dirn = (player.global_position - self.global_position).normalized()
+	arrows.append(arrow)
+	$Marker2D.call_deferred("add_child", arrow)
+	var dirn = distance.normalized()
+	if distance.length() > 30:
+		arrow.linear_velocity = dirn * arrow_speed * 1.5
+	else:
 		arrow.linear_velocity = dirn * arrow_speed
-		print(arrows.size())
-		if arrows.size() > 10:
-			var a = arrows.pop_front()
-			if a:
-				a.queue_free()
-		attack_timer.start()
-		can_attack = false
+	
+	#print(arrows.size())
+	if arrows.size() > 10:
+		var a = arrows.pop_front()
+		if a:
+			a.queue_free()
+
 	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -55,11 +67,12 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 
 
 func _on_attack_state_entered() -> void:
-	can_attack = true
 	var random_archer = "archer" + random_number + "_shoot"
 	sprite.play(random_archer)
-	attack_timer.start()
 	
-func _on_attack_timer_timeout() -> void:
-	can_attack = true
-	shoot_arrow()
+
+
+
+func _on_animated_sprite_2d_frame_changed() -> void:
+	if sprite.animation.ends_with("shoot") and sprite.frame == 8:
+		shoot_arrow()
--- Scripts/arrow.gd
+++ Scripts/arrow.gd
@@ -1,12 +1,13 @@
 extends RigidBody2D
 
 func _ready() -> void:
+	#print(self.get_parent())
 	pass
 	
 func _physics_process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node) -> void:
-	if body.name not in ["Archer", "Arrow", "Ground"]:
+	if body.name not in ["Archer", "Arrow"]:
 		queue_free()
 	#print(body)
